de3deddc18d8aed3a08f95e5f015ce406ef9597e
vlad <vladkalyuzhnyu@gmail.com>
2020-05-08 15:01:42 +0300

    changes

--- android/src/com/mygdx/guessimage/MainActivity.kt
+++ android/src/com/mygdx/guessimage/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.mygdx.guessimage
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.afollestad.recyclical.ViewHolder
 import com.afollestad.recyclical.datasource.dataSourceOf
@@ -18,6 +22,7 @@ import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.kodein.di.generic.instance
+import splitties.dimensions.dip
 import splitties.views.dsl.core.frameLayout
 import splitties.views.dsl.core.lParams
 import splitties.views.dsl.core.matchParent
@@ -36,19 +41,17 @@ class MainActivity : BaseActivity() {
 
     private val dataSource = dataSourceOf("")
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(frameLayout {
             lParams(matchParent, matchParent)
             addView(recyclerView {
                 lParams(matchParent, matchParent)
+                val columns = resources.getInteger(R.integer.columns)
+                addItemDecoration(PuzzleDecoration(applicationContext, columns))
                 setup {
-                    withLayoutManager(
-                        GridLayoutManager(
-                            context,
-                            resources.getInteger(R.integer.columns)
-                        )
-                    )
+                    withLayoutManager(GridLayoutManager(context, columns))
                     withDataSource(dataSource)
                     withItem<String, DummyHolder>(R.layout.item_new) {
                         onBind(::DummyHolder) { _, _ ->
@@ -82,6 +85,28 @@ class MainActivity : BaseActivity() {
                 addAll(items)
                 invalidateAll()
             }
+        }
+    }
+}
+
+class PuzzleDecoration(context: Context, private val columns: Int) : RecyclerView.ItemDecoration() {
+
+    private val minSpace = context.dip(3)
+
+    private val maxSpace = minSpace * 2
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        val position = parent.getChildAdapterPosition(view)
+        outRect.apply {
+            top = if (position / columns == 0) maxSpace else minSpace
+            left = if (position % columns == 0) maxSpace else minSpace
+            right = if ((position + 1) % columns == 0) maxSpace else minSpace
+            bottom = if (position / columns == state.itemCount / columns) maxSpace else minSpace
         }
     }
 }
