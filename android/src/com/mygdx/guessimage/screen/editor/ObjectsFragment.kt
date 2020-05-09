4f621367e076f9f3516e81e92cdd4425259a55a8
vlad <vladkalyuzhnyu@gmail.com>
2020-05-09 22:49:30 +0300

    changes

--- android/src/com/mygdx/guessimage/screen/editor/ObjectsFragment.kt
+++ android/src/com/mygdx/guessimage/screen/editor/ObjectsFragment.kt
@@ -1,28 +1,30 @@
 package com.mygdx.guessimage.screen.editor
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
 import androidx.recyclerview.widget.LinearLayoutManager
-import coil.api.load
+import com.afollestad.recyclical.ViewHolder
 import com.afollestad.recyclical.datasource.dataSourceOf
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
 import com.mygdx.guessimage.local.Database
-import com.mygdx.guessimage.local.entities.PuzzleEntity
-import com.mygdx.guessimage.screen.DummyHolder
-import com.mygdx.guessimage.screen.PuzzleViewHolder
+import com.mygdx.guessimage.local.entities.ObjectEntity
 import com.mygdx.guessimage.screen.base.BaseFragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.kodein.di.generic.instance
-import splitties.views.dsl.core.R
-import splitties.views.dsl.core.matchParent
+import splitties.views.dsl.core.*
 import splitties.views.dsl.recyclerview.recyclerView
+import splitties.views.onClick
+
+class ObjectViewHolder(itemView: View) : ViewHolder(itemView) {
+
+}
 
 class ObjectsFragment : BaseFragment() {
 
@@ -30,45 +32,53 @@ class ObjectsFragment : BaseFragment() {
 
     private val dataSource = dataSourceOf("")
 
+    @Suppress("DEPRECATION")
     override fun onCreateView(inflater: LayoutInflater, root: ViewGroup?, bundle: Bundle?): View {
         return with(activity) {
-            recyclerView {
-                layoutParams = ViewGroup.LayoutParams(matchParent, matchParent)
-                setHasFixedSize(true)
-                setup {
-                    withLayoutManager(LinearLayoutManager(context))
-                    withDataSource(dataSource)
-                    withItem<String, DummyHolder>(R.layout.item_new) {
-                        onBind(::DummyHolder) { _, _ ->
-                        }
-                        onClick { index ->
-                        }
+            verticalLayout {
+                addView(button {
+                    lParams(matchParent, wrapContent)
+                    text = getString(R.string.btn_close)
+                    onClick {
+                        finish()
                     }
-                    withItem<PuzzleEntity, PuzzleViewHolder>(R.layout.item_puzzle) {
-                        onBind(::PuzzleViewHolder) { index, item ->
-                            pictogram.load(item.path)
-                            count.text = "10"
+                })
+                addView(NestedScrollView(applicationContext).apply {
+                    layoutParams = LinearLayout.LayoutParams(matchParent, 0, 1f)
+                    addView(recyclerView {
+                        layoutParams = ViewGroup.LayoutParams(matchParent, wrapContent)
+                        setHasFixedSize(true)
+                        setup {
+                            withLayoutManager(LinearLayoutManager(context))
+                            withDataSource(dataSource)
+                            withItem<ObjectEntity, ObjectViewHolder>(R.layout.item_object) {
+                                onBind(::ObjectViewHolder) { index, item ->
+                                }
+                                onClick { index ->
+                                }
+                            }
                         }
-                        onClick { index ->
-                        }
+                    })
+                })
+                addView(button {
+                    lParams(matchParent, wrapContent)
+                    text = getString(R.string.btn_save)
+                    onClick {
+
                     }
-                }
+                })
             }
         }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK) {
-            when (requestCode) {
-                100 -> launch {
-                    withContext(Dispatchers.IO) {
-                        presenter.getGalleryPath(applicationContext, data?.data ?: return)
-                    }
-                }
+        launch {
+            val items = withContext(Dispatchers.IO) {
+                db.objectDao().getAll()
+            }
+            dataSource.apply {
+                clear()
+                invalidateAll()
             }
         }
     }
