0d2e222730321b702a805a51c850c1a25f8f778b
vlad <vladkalyuzhnyu@gmail.com>
2020-05-10 15:21:49 +0300

    changes

---
+++ core/src/com/mygdx/guessimage/GdxLog.java
@@ -0,0 +1,29 @@
+package com.mygdx.guessimage;
+
+import com.badlogic.gdx.Gdx;
+
+public class GdxLog {
+
+    public static boolean DEBUG;
+
+    @SuppressWarnings("all")
+    public static void print(String tag, String message) {
+        if (DEBUG) {
+            Gdx.app.log(tag, message);
+        }
+    }
+
+    @SuppressWarnings("all")
+    public static void d(String tag, String message, Integer... values) {
+        if (DEBUG) {
+            Gdx.app.log(tag, String.format(message, values));
+        }
+    }
+
+    @SuppressWarnings("all")
+    public static void f(String tag, String message, Float... values) {
+        if (DEBUG) {
+            Gdx.app.log(tag, String.format(message.replaceAll("%f", "%.0f"), values));
+        }
+    }
+}
---
+++ core/src/com/mygdx/guessimage/pool/ScaleToPool.java
@@ -0,0 +1,12 @@
+package com.mygdx.guessimage.pool;
+
+import com.badlogic.gdx.scenes.scene2d.actions.ScaleToAction;
+import com.badlogic.gdx.utils.Pool;
+
+public class ScaleToPool extends Pool<ScaleToAction> {
+
+    @Override
+    protected ScaleToAction newObject() {
+        return new ScaleToAction();
+    }
+}
