b337ddbe7250ddc6651a0221cc81e4f347bf1f84
vlad <vladkalyuzhnyu@gmail.com>
2020-05-10 13:08:48 +0300

    changes

--- android/src/com/mygdx/guessimage/screen/editor/ObjectsFragment.kt
+++ android/src/com/mygdx/guessimage/screen/editor/ObjectsFragment.kt
@@ -9,11 +9,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.afollestad.recyclical.ViewHolder
-import com.afollestad.recyclical.datasource.dataSourceTypedOf
+import com.afollestad.recyclical.datasource.emptyDataSourceTyped
 import com.afollestad.recyclical.setup
 import com.afollestad.recyclical.withItem
 import com.mygdx.guessimage.R
-import com.mygdx.guessimage.extension.nestedScrollView
 import com.mygdx.guessimage.extension.recyclerView
 import com.mygdx.guessimage.local.Database
 import com.mygdx.guessimage.local.entities.ObjectEntity
@@ -29,7 +28,6 @@ import org.jetbrains.anko.verticalLayout
 import org.jetbrains.anko.wrapContent
 import org.kodein.di.generic.instance
 
-
 class ObjectViewHolder(itemView: View) : ViewHolder(itemView) {
     val name: TextView = itemView.tv_name
 }
@@ -40,18 +38,7 @@ class ObjectsFragment : BaseFragment() {
 
     private val db by instance<Database>()
 
-    private val dataSource = dataSourceTypedOf(
-        ObjectEntity().apply { name = "object0" },
-        ObjectEntity().apply { name = "object1" },
-        ObjectEntity().apply { name = "object2" },
-        ObjectEntity().apply { name = "object3" },
-        ObjectEntity().apply { name = "object4" },
-        ObjectEntity().apply { name = "object5" },
-        ObjectEntity().apply { name = "object6" },
-        ObjectEntity().apply { name = "object7" },
-        ObjectEntity().apply { name = "object8" },
-        ObjectEntity().apply { name = "object9" }
-    )
+    private val dataSource = emptyDataSourceTyped<ObjectEntity>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,43 +55,37 @@ class ObjectsFragment : BaseFragment() {
                         activity?.finish()
                     }
                 }.lparams(matchParent, wrapContent)
-                nestedScrollView {
-                    verticalLayout {
-                        layoutParams = ViewGroup.LayoutParams(matchParent, wrapContent)
-                        recyclerView {
-                            isNestedScrollingEnabled = false
-                            addItemDecoration(
-                                DividerItemDecoration(
-                                    context,
-                                    DividerItemDecoration.VERTICAL
-                                )
-                            )
-                            setup {
-                                withLayoutManager(LinearLayoutManager(context))
-                                withDataSource(dataSource)
-                                withItem<ObjectEntity, ObjectViewHolder>(R.layout.item_object) {
-                                    onBind(::ObjectViewHolder) { _, item ->
-                                        name.text = item.name
-                                    }
-                                    onClick { index ->
-                                        puzzleModel.currentObj.value = dataSource[index]
-                                    }
-                                }
+                button {
+                    text = getString(R.string.btn_add)
+                    setOnClickListener {
+                        val obj = ObjectEntity().apply {
+                            name = "object${dataSource.size()}"
+                        }
+                        dataSource.apply {
+                            add(obj)
+                            invalidateAt(size() - 1)
+                        }
+                        puzzleModel.currentObj.value = obj
+                    }
+                }.lparams(matchParent, wrapContent)
+                recyclerView {
+                    addItemDecoration(
+                        DividerItemDecoration(
+                            context,
+                            DividerItemDecoration.VERTICAL
+                        )
+                    )
+                    setup {
+                        withLayoutManager(LinearLayoutManager(context))
+                        withDataSource(dataSource)
+                        withItem<ObjectEntity, ObjectViewHolder>(R.layout.item_object) {
+                            onBind(::ObjectViewHolder) { _, item ->
+                                name.text = item.name
                             }
-                        }.lparams(matchParent, wrapContent)
-                        button {
-                            text = getString(R.string.btn_add)
-                            setOnClickListener {
-                                val obj = ObjectEntity().apply {
-                                    name = "object${dataSource.size()}"
-                                }
-                                dataSource.apply {
-                                    add(obj)
-                                    invalidateAt(size() - 1)
-                                }
-                                puzzleModel.currentObj.value = obj
+                            onClick { index ->
+                                puzzleModel.currentObj.value = dataSource[index]
                             }
-                        }.lparams(matchParent, wrapContent)
+                        }
                     }
                 }.lparams(matchParent, 0, 1f)
                 button {
