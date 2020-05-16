34a664897e4bb7fd878e0c1ef3c157fa8df82896
vlad <vladkalyuzhnyu@gmail.com>
2020-05-16 16:39:19 +0300

    changes

---
+++ core/src/com/mygdx/guessimage/CustomCamera.java
@@ -0,0 +1,26 @@
+package com.mygdx.guessimage;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector3;
+
+public class CustomCamera extends OrthographicCamera {
+
+    private Vector3 lastPosition = new Vector3();
+
+    public CustomCamera() {
+    }
+
+    public CustomCamera(float viewportWidth, float viewportHeight) {
+        super(viewportWidth, viewportHeight);
+    }
+
+    public void setWorldBounds(int left, int bottom, int width, int height) {
+
+    }
+
+    @Override
+    public void translate(float x, float y) {
+        lastPosition.set(position.x, position.y, 0);
+        super.translate(x, y);
+    }
+}
--- core/src/com/mygdx/guessimage/model/Frame.java
+++ core/src/com/mygdx/guessimage/model/Frame.java
@@ -1,44 +1,21 @@
 package com.mygdx.guessimage.model;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Disposable;
 
-public class Frame extends Actor {
-/*
-    private ShapeRenderer sr;
+public class Frame extends Actor implements Disposable {
 
-    public Frame(float width, float weight, Color color){
-        setSize(width,weight);
-        setColor(color);
-
-        sr = new ShapeRenderer();
-        sr.setAutoShapeType(true);
-    }
+    private ShapeRenderer shapeRenderer = new ShapeRenderer();
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        super.draw(batch, parentAlpha);
 
-        batch.end();
+    }
 
-        Vector2 coords = new Vector2(getX(),getY());
-
-        Color color = new Color(getColor());
-        sr.setProjectionMatrix(batch.getProjectionMatrix());
-        sr.setColor(color.r, color.g, color.b, color.a * parentAlpha);
-
-
-
-        Gdx.gl.glEnable(GL20.GL_BLEND);
-        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-        sr.begin(ShapeRenderer.ShapeType.Filled);
-
-
-        sr.rectLine(coords.x,coords.y,coords.x + getWidth(), coords.y, getHeight());
-        sr.end();
-        Gdx.gl.glDisable(GL20.GL_BLEND);
-        Gdx.gl.glLineWidth(1f);
-        sr.setColor(Color.WHITE);
-
-        batch.begin();
-    }*/
+    @Override
+    public void dispose() {
+        shapeRenderer.dispose();
+    }
 }
