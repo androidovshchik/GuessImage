897cf679a79e7748d70904e4b51e56f0dab7f31b
vlad <vladkalyuzhnyu@gmail.com>
2020-05-17 11:16:17 +0300

    changes

--- core/src/com/mygdx/guessimage/CustomCamera.java
+++ core/src/com/mygdx/guessimage/CustomCamera.java
@@ -10,14 +10,20 @@ public class CustomCamera extends OrthographicCamera {
     private static final String TAG = CustomCamera.class.getSimpleName();
 
     private Rectangle imageBounds = new Rectangle();
+    private float imageTop;
+    private float imageRight;
 
     float startZoom = 1f;
+
+    boolean pinchingCamera = false;
 
     public CustomCamera() {
     }
 
-    public void setBackgroundBounds(float width, float height) {
+    public void setImageBounds(float width, float height) {
         imageBounds.set((viewportWidth - width) / 2, (viewportHeight - height) / 2, width, height);
+        imageTop = imageBounds.y + imageBounds.height;
+        imageRight = imageBounds.x + imageBounds.width;
     }
 
     public void setTranslation(float dX, float dY) {
@@ -32,18 +38,25 @@ public class CustomCamera extends OrthographicCamera {
     }
 
     public void normalize() {
-        //imageBounds.overlaps()
         float dx = 0, dy = 0;
         float delta = Gdx.graphics.getDeltaTime();
         float visualWidth = viewportWidth * zoom;
         float visualHeight = viewportHeight * zoom;
         if (visualHeight < imageBounds.height) {
+            if (getTop() > imageTop) {
 
+            } else if (getBottom() < imageBounds.y) {
+
+            }
         } else {
 
         }
         if (visualWidth < imageBounds.width) {
-            imageBounds.fitOutside()
+            if (getLeft() < imageBounds.x) {
+
+            } else if (getRight() > imageRight) {
+
+            }
         } else {
 
         }
@@ -51,5 +64,29 @@ public class CustomCamera extends OrthographicCamera {
             setTranslation(dx, dy);
         }
         //GdxLog.print(TAG, " effectiveViewportWidth " + effectiveViewportWidth + " effectiveViewportHeight " + effectiveViewportHeight);
+    }
+
+    public float getWidth() {
+        return viewportWidth * zoom;
+    }
+
+    public float getHeight() {
+        return viewportHeight * zoom;
+    }
+
+    public float getTop() {
+        return position.y + getHeight() / 2;
+    }
+
+    public float getLeft() {
+        return position.x - getWidth() / 2;
+    }
+
+    public float getRight() {
+        return position.x + getWidth() / 2;
+    }
+
+    public float getBottom() {
+        return position.y - getHeight() / 2;
     }
 }
