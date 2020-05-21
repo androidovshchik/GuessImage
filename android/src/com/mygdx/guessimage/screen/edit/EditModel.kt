34c4631b8cfa7774c09c1fc85dae9ced4b27bd3d
vlad <vladkalyuzhnyu@gmail.com>
2020-05-21 14:40:43 +0300

    changes

--- android/src/com/mygdx/guessimage/screen/edit/EditModel.kt
+++ android/src/com/mygdx/guessimage/screen/edit/EditModel.kt
@@ -4,12 +4,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mygdx.guessimage.local.entities.ObjectEntity
 import com.mygdx.guessimage.local.entities.PuzzleEntity
+import com.mygdx.guessimage.screen.ObjectData
 
 class EditModel : ViewModel() {
 
     lateinit var puzzle: PuzzleEntity
 
-    val currentObj = MutableLiveData<ObjectEntity>()
+    val currentObject = MutableLiveData<ObjectEntity>()
 
     val galleryPath = MutableLiveData<String>()
+
+    val frameChanged = MutableLiveData<ObjectData>()
 }
--- android/src/com/mygdx/guessimage/screen/play/PlayModel.kt
+++ android/src/com/mygdx/guessimage/screen/play/PlayModel.kt
@@ -1,9 +1,12 @@
 package com.mygdx.guessimage.screen.play
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mygdx.guessimage.local.entities.PuzzleEntity
 
 class PlayModel : ViewModel() {
 
     lateinit var puzzle: PuzzleEntity
+
+    val framesGuessed = MutableLiveData<LongArray>()
 }
